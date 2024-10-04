24eef741886d333d63f8c7d00548f3cbaacad0f2
Zack Roth <zack27r@gmail.com>
2024-10-04 15:46:31 -0400

    Removed bootstrap import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
