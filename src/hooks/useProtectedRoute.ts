22c1125b280f58c9ef9144ec4df7a20ecf10cbb5
Zack Roth <zack27r@gmail.com>
2024-10-07 09:41:10 -0400

    updated useProtectedRoute to allow for admin rights

--- src/hooks/useProtectedRoute.ts
+++ src/hooks/useProtectedRoute.ts
@@ -1,11 +1,15 @@
 import { useAuth } from "@/auth/useAuth";
 import { useRouter } from "next/navigation";
 
-export const useProtectedRoute = () => {
+export const useProtectedRoute = (adminRequired?: boolean) => {
   const { user } = useAuth();
   const router = useRouter();
 
   if (user === undefined) {
     router.push("/login");
+  }
+
+  if (adminRequired && user !== "admin") {
+    router.push("/questionnaire-select");
   }
 };
