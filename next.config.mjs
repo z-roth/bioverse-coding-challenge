7fe6239002a9e56cf323a7f260f24e51b9473b21
Zack Roth <zack27r@gmail.com>
2024-10-07 02:48:09 -0400

    Made default page /questionnaire-select

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/questionnaire-select",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
