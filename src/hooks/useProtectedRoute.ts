fb3f862e75f387edcccfb09862526a5cdf2d76a2
Zack Roth <zack27r@gmail.com>
2024-10-07 10:56:03 -0400

    Added comment to useProtectedRoute

--- src/hooks/useProtectedRoute.ts
+++ src/hooks/useProtectedRoute.ts
@@ -1,6 +1,10 @@
 import { useAuth } from "@/auth/useAuth";
 import { useRouter } from "next/navigation";
 
+/**
+ * A React hook to reroute the user to the login screen if they are not logged in.
+ * @param adminRequired if the route should be protected for admins only
+ */
 export const useProtectedRoute = (adminRequired?: boolean) => {
   const { user } = useAuth();
   const router = useRouter();
